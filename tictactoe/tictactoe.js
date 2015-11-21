15550b86fca9569b1d6fd70ed64cec111c5a20bd
sunsplat <heeycutie@yahoo.com>
2015-11-22 06:20:17 +0800

    added cells variable

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,24 +1,41 @@
-var grid = {
-  11: "O",
-  12: "",
-  13: "O",
-  21: "",
-}
+// var grid = {
+//   11: "O",
+//   12: "",
+//   13: "O",
+//   21: "",
+// }
 
-var potentialWins = [[11, 12, 13], [21, 22, 23], [31, 32, 33], [11, 21, 31], [12, 22, 32], [13, 23, 33], [11, 22, 33], [13, 22, 11]];
+// var potentialWins = [[11, 12, 13], [21, 22, 23], [31, 32, 33], [11, 21, 31], [12, 22, 32], [13, 23, 33], [11, 22, 33], [13, 22, 11]];
 
-var openSpace = [];
-function checkForPotentialWins () {
-  // check for 2 O's and an open space
-  for  (i = 0; i < potentialWins.length; i++) {
-      for (y = 0; y < 3; y++) {
-        if (grid[potentialWins[i][y]] !== "X" && potentialWins[i][y] !== "O") {
-          openSpace.push(potentialWins[i][y]);
+// var openSpace = [];
+// function checkForPotentialWins () {
+//   // check for 2 O's and an open space
+//   for  (i = 0; i < potentialWins.length; i++) {
+//       for (y = 0; y < 3; y++) {
+//         if (grid[potentialWins[i][y]] !== "X" && potentialWins[i][y] !== "O") {
+//           openSpace.push(potentialWins[i][y]);
 
-      }
-    }
-  }
-}
+//       }
+//     }
+//   }
+// }
+// http://playtictactoe.org/
+
+var cells = 
+[
+  '#a1',
+  '#a2', 
+  '#a3',
+  '#b1',
+  '#b2',
+  '#b3',
+  '#c1',
+  '#c2',
+  '#c3'
+]
+
+var wins = [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3], [a1, b2, c3], [a3, b2, c1], [a1, b1, c1], [a2, b2, c2], [a3, b3, c3]]
+
 
   $(document).ready(function(){
 
@@ -35,11 +52,6 @@ function checkForPotentialWins () {
     }
   });
   
-
-var wins = {
-
-}
-
 function computerMove() {
   if ($('#b2').text !== "X" || $('#b2').text !== "O") {
     $('#b2').html('O');
