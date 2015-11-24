d09fb5b59717bd89d8cb6cf949ddf83a35afc792
lamp30 <ellen.sun@lampcamp.net>
2015-11-24 14:32:23 -0500

    improved checkwin function. working

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,36 +1,4 @@
-// var grid = {
-//   11: "O",
-//   12: "",
-//   13: "O",
-//   21: "",
-// }
-
-// var potentialWins = [[11, 12, 13], [21, 22, 23], [31, 32, 33], [11, 21, 31], [12, 22, 32], [13, 23, 33], [11, 22, 33], [13, 22, 11]];
-
-// var openSpace = [];
-// function checkForPotentialWins () {
-//   // check for 2 O's and an open space
-//   for  (i = 0; i < potentialWins.length; i++) {
-//       for (y = 0; y < 3; y++) {
-//         if (grid[potentialWins[i][y]] !== "X" && potentialWins[i][y] !== "O") {
-//           openSpace.push(potentialWins[i][y]);
-
-//       }
-//     }
-//   }
-// }
 // http://playtictactoe.org/
-
-//Sets each variable to the text value in the cell
-var a1 = $('#a1').text();
-var a2 = $('#a2').text();
-var a3 = $('#a3').text();
-var b1 = $('#b1').text();
-var b2 = $('#b2').text();
-var b3 = $('#b3').text();
-var c1 = $('#c1').text();
-var c2 = $('#c2').text();
-var c3 = $('#c3').text();
 
 // Create a hash?
 var cells =
@@ -67,20 +35,50 @@ $(document).ready(function(){
     $('.cell').click(function() {
         if (turn % 2 === 0) {
           $(this).html('X');
-        //   checkWin('X');
+          checkWin('X');
         } else {
           $(this).html('O');
-        //   checkWin('O');
+          checkWin('O');
         }
 
         turn++;
     });
 
-    // Checks if the cell in the grid is empty or not
-    function isCellEmpty(value) {
-      if (value.val() !== "O" && value.val() !== "X") {
-        return value;
-      }
+    // After each move, the checkWin function should run to see if the player or computer has won.
+    function checkWin(letter) {
+        //Sets each variable to the text value in the cell
+        var a1 = $('#a1').text();
+        var a2 = $('#a2').text();
+        var a3 = $('#a3').text();
+        var b1 = $('#b1').text();
+        var b2 = $('#b2').text();
+        var b3 = $('#b3').text();
+        var c1 = $('#c1').text();
+        var c2 = $('#c2').text();
+        var c3 = $('#c3').text();
+
+        console.log(c3);
+
+        if (a1 == letter && a2 == letter && a3 == letter) {
+            alert('You won!');
+            newGame();
+        } else if (b1 == letter && b2 == letter && b3 == letter) {
+            alert('You won!');
+            newGame();
+        } else if (c1 == letter && c2 == letter && c3 == letter) {
+            alert('You won!');
+            reset();
+        } else if (a1 == letter && b2 == letter && c3 == letter) {
+            alert('You won!');
+            reset();
+        } else if (a3 == letter && b2 == letter && c1 == letter) {
+            alert('You won!');
+            reset();
+        }
+        // } else {
+        //     alert("It's a draw");
+        //     reset();
+        // }
     }
 
     // Where should the computer move? How should it move?
@@ -93,29 +91,6 @@ $(document).ready(function(){
         $('#a2').html('O');
 
       }
-    }
-
-    // After each move, the checkWin function should run to see if the player or computer has won.
-    function checkWin(letter) {
-        if (a1 == letter && a2 == letter && a3 == letter) {
-            alert('You won!');
-            reset();
-        } else if (b1 == letter && b2 == letter && b3 == letter) {
-            alert('You won!');
-            reset();
-        } else if (c1 == letter && c2 == letter && c3 == letter) {
-            alert('You won!');
-            reset();
-        } else if (a1 == letter && b2 == letter && c3 == letter) {
-            alert('You won!');
-            reset();
-        } else if (a3 == letter && b2 == letter && c1 == letter) {
-            alert('You won!');
-            reset();
-        } else {
-            alert("It's a draw");
-            reset();
-        }
     }
 
     // Clears the board after a game ends. Resets
